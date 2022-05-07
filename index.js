426855a103ce2e2502d6fa515b5b8e17b2803a4a
Mark <marksreese@gmail.com>
2022-05-06 19:05:15 -0700

    commit tree 53419553d90d2c8d371e91c2f54907c208224bd9

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+document.getElementById("main").remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "YOUR-NAME is the champion";
+document.body.append(newHeader);
+
